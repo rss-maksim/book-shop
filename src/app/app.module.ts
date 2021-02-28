10c619db9a4fbed3a0e9c56561aba3f627e1521e
Maksim Kirilko <maksimkirilko@gmail.com>
2021-02-28 17:28:36 +0300

    Task 4: Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,14 @@ import { AppComponent } from './app.component';
 import { BookComponent } from './components/book/book.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
+import { SharedModule } from './shared/shared.module';
+import { BooksModule } from './books/books.module';
+import { CartModule } from './cart/cart.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookComponent,
-    CartComponent,
-    CartItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, BookComponent, CartComponent, CartItemComponent],
+    imports: [BrowserModule, AppRoutingModule, SharedModule, BooksModule, CartModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
